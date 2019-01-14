83f4b02cd0c8c5b04a8f9ac24203d52bc59497dc
Kazu <kazu.hsueh@gmail.com>
2019-01-14 11:14:04 +0800

    init

---
+++ src/main/java/WebApplication.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class WebApplication {
+
+
+}
